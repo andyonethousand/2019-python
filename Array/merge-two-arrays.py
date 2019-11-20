01aa9cadb97a7f42f562f5ae7c96b59e624728a6
john-s-chen <54973763+john-s-chen@users.noreply.github.com>
2019-11-19 19:48:43 -0800

    Update merge-two-arrays.py

--- Array/merge-two-arrays.py
+++ Array/merge-two-arrays.py
@@ -1,42 +1,3 @@
-<<<<<<< HEAD
-# Definition for singly-linked list.
-# class ListNode(object):
-#     def __init__(self, x):
-#         self.val = x
-#         self.next = None
-
-class Solution(object):
-    def mergeTwoLists(self, l1, l2):
-        """
-        :type l1: ListNode
-        :type l2: ListNode
-        :rtype: ListNode
-        """
-        
-        prehead = ListNode(-1)
-        temp = prehead
-        
-        while l1 and l2:
-        
-            if l2.val > l1.val:
-                temp.next = l1
-                temp = l1
-                l1 = l1.next
-            
-            else:
-                temp.next = l2
-                temp = l2
-                l2 = l2.next
-        
-        if l1 == None:
-            temp.next = l2
-        
-        if l2 == None:
-            temp.next = l1
-            
-        return prehead.next
-=======
-  
 def merge(a, b):
   start_index = len(b) - len(a)
   for i in a:
@@ -68,4 +29,3 @@ def merge(a, b):
       a_index -= 1
       end -= 1
   return b
->>>>>>> parent of 7ddf2b3... Update merge-two-arrays.py
