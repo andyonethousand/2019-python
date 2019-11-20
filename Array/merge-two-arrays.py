b903142f77b3b87eb2f28cfd0c0326754b134d1b
john-s-chen <54973763+john-s-chen@users.noreply.github.com>
2019-11-19 20:46:16 -0800

    Update merge-two-arrays.py

--- Array/merge-two-arrays.py
+++ Array/merge-two-arrays.py
@@ -1,24 +1,14 @@
-def merge(a, b):
-  a_index = len(a) - 1
-  b_index = len(b) - len(a) - 1
-  end = -1
-  while a_index >= 0 and b_index >= 0:
-    if a[a_index] > b[b_index]:
-      b[end] = a[a_index]
-      a_index -= 1
-      end -= 1
-    else:
-      b[end] = b[b_index]
-      b_index -= 1
-      end -= 1
-  if a_index < 0:
-    for i in range(0, b_index):
-      b[end] = b[b_index]
-      b_index -= 1
-      end -= 1
-  else:
-    for i in range(0, a_index):
-      a[end] = a[a_index]
-      a_index -= 1
-      end -= 1
-  return b
+class Solution:
+    def merge(self, nums1: List[int], m: int, nums2: List[int], n: int) -> None:
+        """
+        Do not return anything, modify nums1 in-place instead.
+        """        
+        while n > 0:
+            if m <= 0 or nums2[n - 1] >= nums1[m - 1]:
+                nums1[m + n - 1] = nums2[n - 1]
+                n -= 1
+            else:
+                nums1[m + n - 1] = nums1[m - 1]
+                m -= 1
+        
+   
